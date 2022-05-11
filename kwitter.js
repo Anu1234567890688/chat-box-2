dfc79e84557544ba29fb95e68d0faa57661628cf
Anu1234567890688 <89580648+Anu1234567890688@users.noreply.github.com>
2022-05-11 18:31:14 +0530

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -2,7 +2,7 @@ function addUser()
 {
     user_name = document.getElementById("user_name").value;
 
-    localStorage.storage.setItem("user_name", user_name);
+    localStorage.setItem("user_name", user_name);
 
     window.location = "kwitter_room.html";
 }
